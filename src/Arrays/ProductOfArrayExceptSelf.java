68adeb548d52e4e24cc2a8afc2a9ff91e3dbbfbb
Sai Chinmay Tripurari <47316906+saichinmay2000@users.noreply.github.com>
2025-01-18 23:50:21 +0530

    Prod Array Sum

--- src/Arrays/ProductOfArrayExceptSelf.java
+++ src/Arrays/ProductOfArrayExceptSelf.java
@@ -20,9 +20,6 @@ public class ProductOfArrayExceptSelf {
         return res;
     }
 
-
-
-
     public static void main(String[] args) {
         int[] arr = {10, 3, 5, 6, 2};
         int[] res = productExceptSelf(arr);
